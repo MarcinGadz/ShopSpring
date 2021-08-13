417b708b4e961cc3076861c042434b6fe0a5041f
marcingadz <marcingadz@gmail.com>
2021-08-14 01:35:15 +0200

    Created template rest controller and refactored ItemApiController to use it

--- src/main/java/com/mg/demo/controller/ItemApiController.java
+++ src/main/java/com/mg/demo/controller/ItemApiController.java
@@ -2,62 +2,27 @@ package com.mg.demo.controller;
 
 import com.mg.demo.entity.Item;
 import com.mg.demo.service.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+
 
 @RestController
 @RequestMapping("/items")
-public class ItemApiController {
+public class ItemApiController extends ApiTemplateController<Item> {
 
     private Service<Item> service;
 
-    private final Logger logger = LoggerFactory.getLogger(ItemApiController.class.getName());
-
     @Autowired
-    public ItemApiController(Service<Item> itemService) {
-        this.service = itemService;
+    public ItemApiController(Service<Item> service) {
+        this.service = service;
     }
 
-    @GetMapping
-    public List<Item> getAll() {
-        logger.info("getting all items");
-        return service.getAll();
-    }
-
-    @GetMapping("/{id}")
-    public Item getOne(@PathVariable long id) {
-        logger.info("Getting by id: " + id);
-        return service.findById(id);
-    }
-
-    @PostMapping
-    public Item save(@RequestBody Item item) {
-        logger.info("Saving: " + item);
-        service.add(item);
-        logger.info("Saved: " + item);
-        return item;
-    }
-
-    @PutMapping
-    public Item update(@RequestBody Item item) {
-        if (service.existsById(item.getId())) {
-            logger.info("Updating existing item by: " + item);
-            service.update(item);
-            return item;
-        }
-        logger.info("Item with id " + item.getId() + " do not exists");
-        //TODO throw proper exception
-        return null;
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable long id) {
-        logger.info("Deleting by id: " + id);
-        service.deleteById(id);
+    @PostConstruct
+    private void setSuperService() {
+        //TODO try to find better solution
+        super.setService(service);
     }
 
 }
