4eeee6905f40677e5e9915664cc6fc11f7be036a
marcingadz <marcingadz@gmail.com>
2021-08-21 00:46:18 +0200

    Added password encoding

--- src/main/java/com/mg/demo/service/UserService.java
+++ src/main/java/com/mg/demo/service/UserService.java
@@ -3,11 +3,29 @@ package com.mg.demo.service;
 import com.mg.demo.dao.UserDAO;
 import com.mg.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 @org.springframework.stereotype.Service
 public class UserService implements Service<User> {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+    private PasswordEncoder encoder;
+
+    public PasswordEncoder getEncoder() {
+        return encoder;
+    }
+
+    @Autowired
+    public void setEncoder(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
 
     private UserDAO dao;
 
@@ -33,11 +51,13 @@ public class UserService implements Service<User> {
 
     @Override
     public User add(User obj) {
+        obj.setPasswordHash(getEncoder().encode(obj.getPasswordHash()));
         return dao.save(obj);
     }
 
     @Override
     public User update(User obj) {
+        obj.setPasswordHash(getEncoder().encode(obj.getPasswordHash()));
         return dao.save(obj);
     }
 
